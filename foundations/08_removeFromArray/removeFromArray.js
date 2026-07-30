167c99a2f517bf40393fbfe80a6caafe459547a6
Ahad <206739803+Ahad-n@users.noreply.github.com>
2026-07-30 10:51:03 +0300

    Complete exercise 08_removeFromArray.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeItems) {
+    for (let item of removeItems) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === item) {
+                arr.splice(arr.indexOf(item), 1);
+                i--;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
