297bd261c16606964c7eb51264bb15cabf55933d
Ahad <206739803+Ahad-n@users.noreply.github.com>
2026-08-02 11:50:50 +0300

    Implement fundTheOldest function.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(objArray) {
+    return objArray.reduce((lastPerson, nextPerson) => {
+        if (lastPerson.yearOfDeath === undefined) lastPerson.yearOfDeath = (new Date).getFullYear();
+        if (nextPerson.yearOfDeath === undefined) nextPerson.yearOfDeath = (new Date).getFullYear();
 
+
+        return (lastPerson.yearOfDeath - lastPerson.yearOfBirth) 
+             > (nextPerson.yearOfDeath - nextPerson.yearOfBirth)
+            ? lastPerson : nextPerson});
 };
 
 // Do not edit below this line
