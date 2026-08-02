f0f5cab75bc74f6548e63e25eb4887701b623de1
Ahad <206739803+Ahad-n@users.noreply.github.com>
2026-08-02 10:54:31 +0300

    Fill the palindromes function.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const isAlpha = letter => /^[a-z]+$/i.test(letter);
+    let strArr = str.split('');
+    strArr = strArr.filter(letter => isAlpha(letter) || Number(letter));
+    strArr = strArr.map(letter => letter.toUpperCase());
+    return strArr.join('') === strArr.reverse().join('');
 };
 
 // Do not edit below this line
