682b89b1b0a01aa60d9642977b1df6b5d68f5b97
Ahad <206739803+Ahad-n@users.noreply.github.com>
2026-08-02 10:18:24 +0300

    Complete calculator logic.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, currentNumber) => total + currentNumber, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentNumber) => accumulator * currentNumber);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	while(true) {
+    if (num == 0) break;
+    factorial *= num;
+    num--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
